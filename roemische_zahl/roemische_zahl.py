c6cab1e8299f29beff8a1ce326ce09ee2542b03a
Martin Fischer <martin.jan.fischer@arcor.de>
2023-11-20 18:30:57 +0100

    Römische Zahlen von 1 bis 1000

--- roemische_zahl/roemische_zahl.py
+++ roemische_zahl/roemische_zahl.py
@@ -1,9 +1,60 @@
 def berechne_roemische_zahl(arabische_zahl):
     roemische_zahl=""
-    if arabische_zahl==7:
-        roemische_zahl="VII"
-    if arabische_zahl==1:
-        roemische_zahl="I"
+
+
+    while arabische_zahl>=1000:
+        arabische_zahl -= 1000
+        roemische_zahl += "M"
+
+    # Zahlen 100 bis 999
+
+    if arabische_zahl>=900:
+        roemische_zahl+="CM"
+        arabische_zahl -= 900
+
+    if arabische_zahl>=500:
+        arabische_zahl -= 500
+        roemische_zahl+="D"
+    if arabische_zahl>=400:
+        arabische_zahl -= 400
+        roemische_zahl+="CD"
+
+    while arabische_zahl>=100:
+        arabische_zahl -= 100
+        roemische_zahl += "C"
+
+    # Zahlen 10 bis 99
+    if arabische_zahl>=90:
+        roemische_zahl+="XC"
+        arabische_zahl -= 90
+
+    if arabische_zahl>=50:
+        arabische_zahl -= 50
+        roemische_zahl+="L"
+    if arabische_zahl>=40:
+        arabische_zahl -= 40
+        roemische_zahl+="XL"
+
+    while arabische_zahl>=10:
+        arabische_zahl -= 10
+        roemische_zahl += "X"
+
+    # Zahlen 1 bis 9
+    if arabische_zahl>=9:
+        roemische_zahl+="IX"
+        arabische_zahl -= 9
+
+    if arabische_zahl>=5:
+        arabische_zahl -= 5
+        roemische_zahl+="V"
+    if arabische_zahl>=4:
+        arabische_zahl -= 4
+        roemische_zahl+="IV"
+
+    while arabische_zahl>=1:
+        arabische_zahl -= 1
+        roemische_zahl += "I"
+
     return roemische_zahl
 
 
@@ -16,4 +67,5 @@ if __name__ == '__main__':      # Definiere die __main__ Funktion
     # Die obige Funktion "berechne_roemische_zahl" 
     # wird nur dann in der Kommandozeile ausgeführt...
     # ... wenn man sie hier in der __main__ Funktion aufruft
-    print(berechne_roemische_zahl(args.arabische_zahl))
+    roemische_zahl = berechne_roemische_zahl(args.arabische_zahl)
+    print(roemische_zahl)
