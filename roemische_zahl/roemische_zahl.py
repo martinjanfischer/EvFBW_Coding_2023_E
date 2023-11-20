8ae107017744f5aa899701dcee27c3abea0b1d20
Martin Fischer <martin.jan.fischer@arcor.de>
2023-11-20 17:08:17 +0100

    Initial commit

--- roemische_zahl/roemische_zahl.py
+++ roemische_zahl/roemische_zahl.py
@@ -1,5 +1,12 @@
+def berechne_roemische_zahl(arabische_zahl):
+    roemische_zahl=""
+    if arabische_zahl==7:
+        roemische_zahl="VII"
+    if arabische_zahl==1:
+        roemische_zahl="I"
+    return roemische_zahl
 
-'''
+
 if __name__ == '__main__':      # Definiere die __main__ Funktion
     import argparse
     parser = argparse.ArgumentParser(description = 'Berechne die Römische Zahl aus einer natürlichen Arabischen Zahl')
@@ -9,4 +16,4 @@ if __name__ == '__main__':      # Definiere die __main__ Funktion
     # Die obige Funktion "berechne_roemische_zahl" 
     # wird nur dann in der Kommandozeile ausgeführt...
     # ... wenn man sie hier in der __main__ Funktion aufruft
-'''
+    print(berechne_roemische_zahl(args.arabische_zahl))
