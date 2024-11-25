b54a1e81008c8397e41a56338be817a2fcfe6bfc
Martin Fischer <martin.fischer@ubisoft.com>
2024-11-25 21:27:37 +0100

    Algorithmen und Datenstrukturen CH2 gcd gcd_modulo

--- AlgorithmenDatenstrukturenCpp/RS_CH2_GCD/RS_CH2_GCD.cpp
+++ AlgorithmenDatenstrukturenCpp/RS_CH2_GCD/RS_CH2_GCD.cpp
@@ -2,20 +2,50 @@
 
 using namespace std;
 
-int gcd(int u, int v)
+int gcd(int u, int v, int& anzahl_schritte)
 {
-	int t;
+	anzahl_schritte = 0;
 	while (u > 0)
 	{
-		if (u < v) { t = u; u = v; v = t; }
+		if (u < v) { int t = u; u = v; v = t; }
 		u = u - v;
+		anzahl_schritte += 1;
 	}
 	return v;
 }
-main()
+
+int gcd_modulo(int u, int v, int& anzahl_schritte)
 {
-	int x, y;
-	while (cin >> x && cin >> y)
-		if (x > 0 && y > 0)
-			cout << x << ' ' << y << ' ' << gcd(x, y) << endl;
+	anzahl_schritte = 0;
+	while (u > 0)
+	{
+		if (u < v) { int t = u; u = v; v = t; }
+		u = u % v;
+		anzahl_schritte += 1;
+	}
+	return v;
+}
+
+void main()
+{
+	int u = 0;
+	cout << "u: ";
+	cin >> u;
+	cout << endl;
+
+	int v = 0;
+	cout << "v: ";
+	cin >> v;
+
+	if (u > 0 && v > 0)
+	{
+		int teiler, anzahl_schritte;
+		anzahl_schritte = 0;
+		teiler = gcd(u, v, anzahl_schritte);
+		cout << u << ' ' << v << ' ' << teiler << ' ' << anzahl_schritte << endl;
+
+		anzahl_schritte = 0;
+		teiler = gcd_modulo(u, v, anzahl_schritte);
+		cout << u << ' ' << v << ' ' << teiler << ' ' << anzahl_schritte << endl;
+	}
 }
