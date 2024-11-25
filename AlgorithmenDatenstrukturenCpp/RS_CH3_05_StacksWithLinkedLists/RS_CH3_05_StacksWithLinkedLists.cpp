be4eb80c1d63a8d835b80255fb36304a920230f1
Martin Fischer <martin.fischer@ubisoft.com>
2024-11-25 22:39:53 +0100

    Algorithmen und Datenstrukturen CH3 05 Stacks

--- AlgorithmenDatenstrukturenCpp/RS_CH3_05_StacksWithLinkedLists/RS_CH3_05_StacksWithLinkedLists.cpp
+++ AlgorithmenDatenstrukturenCpp/RS_CH3_05_StacksWithLinkedLists/RS_CH3_05_StacksWithLinkedLists.cpp
@@ -4,31 +4,63 @@ typedef int itemType;
 
 class Stack
 {
-private:
-	itemType* stack;
-	int p;
 public:
-	Stack(int max = 100)
-	{
-		stack = new itemType[max]; p = 0;
-	}
-	~Stack()
-	{
-		delete stack;
-	}
-	inline void push(itemType v)
-	{
-		stack[p++] = v;
-	}
-	inline itemType pop()
-	{
-		return stack[--p];
-	}
-	inline int empty()
-	{
-		return !p;
-	}
+    Stack(int max);
+    ~Stack();
+    void push(itemType v);
+    itemType pop();
+    int empty();
+private:
+    struct node
+    {
+        itemType key;
+        struct node* next;
+    };
+    struct node* head;
+    struct node* z;
 };
+
+Stack::Stack(int max)
+{
+    head = new node;
+    z = new node;
+    head->next = z;
+    z->next = z;
+}
+
+Stack::~Stack()
+{
+    struct node* t = head;
+    while (t != z)
+    {
+        head = t;
+        t = t->next;
+        delete head;
+    }
+}
+
+void Stack::push(itemType v)
+{
+    struct node* t = new node;
+    t->key = v;
+    t->next = head->next;
+    head->next = t;
+}
+
+itemType Stack::pop()
+{
+    itemType x;
+    struct node* t = head->next;
+    head->next = t->next;
+    x = t->key;
+    delete t;
+    return x;
+}
+
+int Stack::empty()
+{
+    return head->next == z;
+}
 
 using namespace std;
 
--- AlgorithmenDatenstrukturenCpp/sedgewick_sources.cpp
+++ AlgorithmenDatenstrukturenCpp/sedgewick_sources.cpp
@@ -5,47 +5,6 @@ CHAPTER 3 Elementary Data Structures
  
     key[x] = v; next[x] = next[t]; next[t] = x++;
  
-class Stack 
-  {
-    public:
-      Stack(int max); 
-     ~Stack();
-      void push(itemType v);
-      itemType pop();
-      int empty();
-    private:
-      struct node 
-       { itemType key; struct node *next; };
-      struct node *head, *z;
-  };
- 
-Stack::Stack(int max) 
-  {
-    head = new node; z = new node;
-    head->next = z;  z->next = z;
-  }
-Stack::~Stack() 
-  { 
-    struct node *t = head;
-    while (t != z) 
-      { head = t; t = t->next; delete head; }
-  }
- 
-void Stack::push(itemType v) 
-  {
-    struct node *t = new node;
-    t->key = v; t->next = head->next; 
-    head->next = t;
-  }
-itemType Stack::pop()   
-  {
-    itemType x;
-    struct node *t = head->next; 
-    head->next = t->next; x = t->key;
-    delete t; return x;
-  }
-int Stack::empty()
-  { return head->next == z; }
  
 void Queue::put(itemType v)
   {
